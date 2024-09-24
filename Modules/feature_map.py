f4d3b51d8901773a29be0fe0f3f097562aa9e9e7
youssefibrahimm <youssefIbrahim0@aucegypt.edu>
2024-09-24 20:45:27 +0300

    modified:   Modules/__pycache__/Dynamic_dec.cpython-39.pyc 	modified:   Modules/__pycache__/feature_map.cpython-39.pyc 	modified:   Modules/feature_map.py
    	modified:   Modules/MPVConv (new commits)
    	modified:   Modules/feature_map.py

--- Modules/feature_map.py
+++ Modules/feature_map.py
@@ -1,3 +1,8 @@
+import sys
+# add your path to the sys 
+sys.path.insert(0, 'C:\\Users\\youss\\OneDrive - aucegypt.edu\\Youssef\\3D_recon_pc')
+print(sys.path)
+
 import torch.nn as nn
 import torch
 from Modules.MPVConv.models.s3dis.mpvcnnpp import MPVCNN2
