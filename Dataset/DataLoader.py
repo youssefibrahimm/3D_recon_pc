0bc72d73c1aad1dc7c5240b95cdc07975f49d8c8
youssefibrahimm <youssefIbrahim0@aucegypt.edu>
2024-10-17 22:29:34 +0300

    modified:   Dataset/DataLoader.py

--- Dataset/DataLoader.py
+++ Dataset/DataLoader.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset, DataLoader, random_split
 import open3d as o3d
 import numpy as np
+import torch
 class Read_ply(Dataset):
     def __init__(self, ply_path):
         super(Read_ply, self).__init__()
@@ -14,16 +15,22 @@ class Read_ply(Dataset):
         return points, points
 
     def collate__fn(self, points):
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
         features = np.stack([p[0] for p in points], axis = 0) # (B, 3, N)
         coordintates = np.stack([p[1] for p in points], axis = 0) # (B, 3, N)
-        return features, coordintates  
+        f = torch.tensor(features).float().to(device)
+        c = torch.tensor(coordintates).float().to(device)
+        pad_channels = 9 - f.size(1) - c.size(1)  # Calculate how many extra channels are needed
+        padding = torch.zeros(f.size(0), pad_channels, f.size(2), device=device)  # Create padding tensor
+        data = torch.cat((f, c, padding), dim=1).to(device)
+        return data  
 
 def RandomSplit(datasets, train_set_percentage):
     lengths = [int(len(datasets)*train_set_percentage), len(datasets)-int(len(datasets)*train_set_percentage)]
     return random_split(datasets, lengths)
 
 
-def GetDataLoader(ply_path, batch_size, train_set_percentage=0.9,shuffle=True, drop_last=True):
+def GetDataLoader(ply_path, batch_size, train_set_percentage=0.9, shuffle=True, drop_last=True):
     # Defining the dataset
     ds = Read_ply(ply_path)
     
