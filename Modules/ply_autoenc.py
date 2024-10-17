1950228b29006194eff30aa9d65179eb3df8441b
youssefibrahimm <youssefIbrahim0@aucegypt.edu>
2024-10-17 22:52:33 +0300

    modified:   Modules/ply_autoenc.py

--- Modules/ply_autoenc.py
+++ Modules/ply_autoenc.py
@@ -13,7 +13,7 @@ class AE_ply(nn.Module):
     self.n_embed = n_embed
     self.kernel_size = kernel_size
     self.mpvConv1 = MPVConv(3+latent_size, 64, resolution=32, kernel_size=self.kernel_size)
-    self.mpvConv2 = MPVConv(64, 128, resolution=32, kernel_size=1)
+    self.mpvConv2 = MPVConv(64, 128, resolution=32, kernel_size=self.kernel_size)
     self.mpvConv3 = MPVConv(128, latent_size, resolution=32, kernel_size=self.kernel_size)
 
     # Linear layer to project latent_size to n_embed
