492278655deec50dcc0e363f73c9ab01c04753ee
youssefibrahimm <youssefIbrahim0@aucegypt.edu>
2024-10-18 18:50:18 +0300

    modified:   Modules/feature_map.py 	modified:   Modules/ply_autoenc.py

--- Modules/feature_map.py
+++ Modules/feature_map.py
@@ -10,29 +10,36 @@ from ply_autoenc import AE_ply
 
 
 class feature_map_AE(nn.Module):
-  def __init__(self, latent_size, num_of_feat, n_embed, kernel_size,width_multiplier):
-    """ 
-    Initializes the feature_map_AE module.
+  """
+  Initializes the feature_map_AE module.
 
-    Parameters:
-        latent_size (int): The size of the latent space.
-        num_of_feat (int): The number of features.
-        n_embed (int): The embedding size.
-        width_multiplier (int): The width multiplier.
+  Parameters:
+      latent_size (int): The size of the latent space.
+      num_of_feat (int): The number of features.
+      n_embed (int): The embedding size.
+      width_multiplier (int): The width multiplier.
+      num_points (int): The number of points in the input point cloud.
+      isConv (bool): Whether to use convolutional layers for projection to latent_size, or linear layers.
 
-    Attributes:
-        latent_size (int): The size of the latent space.
-        mpvcnn (MPVCNN2): The MPVCNN2 instance.
-        linear (nn.Linear): Linear layer for the last layer.
-        layer_norm (nn.LayerNorm): Layer normalization instance.
-        Auto_enc (AE_ply): The AE_ply instance.
+  Attributes:
+      latent_size (int): The size of the latent space.
+      mpvcnn (MPVCNN2): The MPVCNN2 instance.
+      linear (nn.Linear): Linear layer for the last layer.
+      layer_norm (nn.LayerNorm): Layer normalization instance.
+      Auto_enc (AE_ply): The AE_ply instance.
 
-    """
+  """
+  def __init__(self, latent_size, num_of_feat, n_embed, kernel_size, width_multiplier, num_points, isConv=True):
     super(feature_map_AE, self).__init__()
+    assert kernel_size>=3, f"kernel_size shoud be at least 3, but got {kernel_size}"
+
     self.latent_size = latent_size
+    self.num_points = num_points
+    self.isConv = isConv
     self.mpvcnn = MPVCNN2(num_of_feat, width_multiplier=width_multiplier)
     # adding a new last layer
     self.linear = nn.Linear(num_of_feat*width_multiplier, latent_size)
+    self.conv1x1 = nn.Conv1d(num_of_feat*width_multiplier, out_channels=1, kernel_size=kernel_size, stride=(num_points//self.latent_size))
     self.layer_norm = nn.LayerNorm(latent_size)
     self.Auto_enc = AE_ply(latent_size=latent_size, n_embed=n_embed, kernel_size=kernel_size)
 
@@ -40,14 +47,22 @@ class feature_map_AE(nn.Module):
     # ply should be a tuple containig features (Batch_size, Channel_in, Num_points) and coords (Batch_size, 3, Num_points)
     features, coords = ply, ply[:,:3,:].contiguous()
     features_1 = self.mpvcnn(features) # (batch_size, num_of_feat, num_of_points)
-    print(f'feature_2 after mean:{features_1.shape}')
-    features_2 = features_1.mean(dim=2) # (batch_size, num_of_feat)
+    print(f'feature_2 before :{features_1.shape}')
 
-    # adjusting the shape of the output to pass to the autoencoder
-    # Normalize and project to latent_size
-    print(f'feature_2 after mean:{ features_2.shape}')
-    linear_layer = self.linear(features_2)
-    print(f'linear_layer:{ linear_layer.shape}')
+    if self.isConv:
+      features_1 = self.conv1x1(features_1).squeeze(1) # (batch_size, latent-size)
+      print(f'conv layer output shape :{features_1.shape}')
+    else:
+      pred_points=[]
+      for feat_idx in range(features_1.shape[1]):
+        feat_pred = features_1[:,feat_idx,:]
+        pred_points.append(feat_pred) 
+
+      features_2 = torch.mean(torch.stack(pred_points), dim=0) # (batch_size, num_of_points)
+      # project to latent_size
+      linear_layer = self.linear(features_2)
+      print(f'linear_layer:{ linear_layer.shape}')
+
     feat_3 = self.layer_norm(linear_layer).unsqueeze(2) # (batch_size, latent_size, 1)
     print(f'feat_3:{feat_3.shape}')
     features_3 = feat_3.expand(-1, -1, features_1.size(2)) # (batch_size, latent_size, num_of_points)
@@ -56,6 +71,7 @@ class feature_map_AE(nn.Module):
     # Combine the coordinates with the features
     # coords_expanded = coords.unsqueeze(1).expand(-1, 3, -1) # (batch_size, 3, num_of_points)
     features_3 = torch.cat([coords, features_3], dim=1)  # (batch_size, 3+latent_size, num_of_points)
+    print(f'features_3_cat:{features_3.shape}')
     return features_3, coords 
   
   def forward(self, ply):
--- Modules/ply_autoenc.py
+++ Modules/ply_autoenc.py
@@ -6,6 +6,11 @@ sys.path.insert(0, 'C:\\Users\\youss\\OneDrive - aucegypt.edu\\Youssef\\3D_recon
 from MPVConv.modules.mpvconv import MPVConv
 from Transformer_parts import encoderBlock
 import torch.nn as nn
+
+class Transpose_layer(nn.Module):
+  def forward(self,x):
+    return x.transpose(1,2)
+    
 class AE_ply(nn.Module):
   def __init__(self, latent_size, n_embed, kernel_size):
     super(AE_ply, self).__init__()  
@@ -16,8 +21,19 @@ class AE_ply(nn.Module):
     self.mpvConv2 = MPVConv(64, 128, resolution=32, kernel_size=self.kernel_size)
     self.mpvConv3 = MPVConv(128, latent_size, resolution=32, kernel_size=self.kernel_size)
 
-    # Linear layer to project latent_size to n_embed
-    self.latent_to_embed = nn.Linear(self.latent_size, self.n_embed)
+    
+    self.downsampling_NumPoints = nn.Sequential(
+    # input shape (batch_size, n_embed, Num_points)
+    Transpose_layer(),
+    nn.Linear(self.latent_size, self.n_embed),
+    Transpose_layer(),
+    nn.Conv1d(in_channels=self.n_embed, out_channels=64, kernel_size=kernel_size, stride=10),
+    nn.AvgPool1d(kernel_size=kernel_size, stride=2),
+    nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3, stride=10),
+    nn.AvgPool1d(kernel_size=kernel_size, stride=2),
+    Transpose_layer(),
+    nn.Linear(in_features=64, out_features=n_embed)
+    )
 
     # Encoder block expecting (batch_size, num_points, n_embed)
     self.encoder = encoderBlock(n_embed=self.n_embed, num_heads=2)
@@ -29,8 +45,7 @@ class AE_ply(nn.Module):
     x, coords = self.mpvConv1(input)
     x, coords = self.mpvConv2((x, coords))
     x, _ = self.mpvConv3((x,coords)) # (batch_size, latent_size, Num_points)
-    x = x.transpose(1,2) # (batch_size, Num_points, latent_size)
-    x = self.latent_to_embed(x) # (batch_size, Num_points, n_embed)
+    x = self.downsampling_NumPoints(x) # (batch_size, Num_points=11597, n_embed)
     x, k_enc, v_enc = self.encoder(x) # (batch_size, Num_points, n_embed)
     x = self.embed_to_latent(x) # (batch_size, Num_points, latent_size)
     return x, k_enc, v_enc
