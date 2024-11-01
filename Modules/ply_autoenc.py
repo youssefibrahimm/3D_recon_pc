0cf3681a0b161f433c9e723dfff25ef13382c703
youssefibrahimm <youssefIbrahim0@aucegypt.edu>
2024-11-01 16:01:04 +0200

    modified:   Modules/ply_autoenc.py

--- Modules/ply_autoenc.py
+++ Modules/ply_autoenc.py
@@ -55,6 +55,5 @@ class AE_ply(nn.Module):
     x, coords = self.mpvConv2((x, coords))
     x, _ = self.mpvConv3((x,coords)) # (batch_size, latent_size, Num_points)
     out_down = self.downsampling_NumPoints(x) # (batch_size, Num_points=11597, n_embed)
-    print(f'auto_enc before encoder: {out_down.shape}')
     enc_out, k_enc, v_enc = self.encoder(out_down) # (batch_size, Num_points, n_embed)
     return enc_out, k_enc, v_enc, enc_out.shape[1]
